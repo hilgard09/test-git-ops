8cea3ef55cb13bc4473b73b1d198d78621944b72
aquario <genghy@gmail.com>
2014-06-10 13:52:25 -0700

    integer sum

--- src/hello.cc
+++ src/hello.cc
@@ -2,5 +2,13 @@
 
 int main() {
   std::cout << "hello world~!!" << std::endl;
+
+  int s = 0;
+  for (int i = 0; i < 11; i++) {
+    s += i;
+  }
+
+  std::cout << "Sum = " << s << std::endl;
+
   return 0;
 }
